b1f17dd854427f69f96dfd9f676b0afa0e70f4e1
AyseGizemSahbaz <@sahbazgzm@gmail.com>
2023-01-26 19:38:42 +0300

    task 5 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -4,7 +4,7 @@ public class Student {
     public static void main(String[] args) {
 
         System.out.println("merhaba 1");
-        System.out.println("task 2 yapıldı");
+
         System.out.println("tekrar merhaba ");
     }
 }
